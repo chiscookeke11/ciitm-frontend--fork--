99df735a2364a94262760d16656d77c6e4a6fe33
chiscookeke11 <chiscookeke11@gmail.com>
2025-02-05 01:34:34 -1100

    new fix

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { backend_Url } from './src/utils/constants';
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: backend_Url, // Proxy API requests to backend
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+   plugins: [react()],
+   server: {
+      proxy: {
+         '/api': backend_Url,
+         // '/api': 'http://localhost:3000',
       },
-    },
-  },
-  build: {
-    outDir: 'dist', // Default output directory for Vite
-    sourcemap: true, // Enable source maps for debugging
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Shorten import paths using alias
-    },
-  },
+   },
 });
